00d3eeb5ab8fa806deac8db333ff88901fd9d3ce
GraySliver <568756361@qq.com>
2018-03-05 14:53:23 +0800

    update test

--- test/test_wencai.py
+++ test/test_wencai.py
@@ -1,9 +1,8 @@
 import unittest
 import wencai as wc
 
-
-
 class TestWenCai(unittest.TestCase):
+
     def test_get_scrape_transaction(self):
         r = wc.get_scrape_transaction('跳空高开,平台突破,非涨停,股价大于ma120,ma30ma120ma250上移,股价大于前30日最高价,非银行版块')
         print(r)
@@ -15,3 +14,6 @@ class TestWenCai(unittest.TestCase):
     def test_get_scrape_report(self):
         r=  wc.get_scrape_report('跳空高开,平台突破,非涨停,股价大于ma120,ma30ma120ma250上移,股价大于前30日最高价,非银行版块')
         print(r)
+
+if __name__ == '__main__':
+    unittest.main()
