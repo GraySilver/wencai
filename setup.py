83ec0b65189e9a7079524bfbca40eccbfec32022
GraySilver <568756361@qq.com>
2021-09-11 17:13:47 +0800

    [fix] fix bug by ignore js.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ else:
 
 setup(
     name='wencai',
-    version='0.2.5',
+    version='0.2.6',
     author='allen yang',
     author_email='allenyzx@163.com',
     url='https://upload.pypi.org/allenyzx/',
